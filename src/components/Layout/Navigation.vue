4353447746fb78efde8cb3d2e5f49cd76ce80ccd
Herman <smurovnemez@mail.ru>
2022-08-21 02:55:55 +0300

    feat: navbar responsive mobile screen

--- src/components/Layout/Navigation.vue
+++ src/components/Layout/Navigation.vue
@@ -2,10 +2,10 @@
   <div class="navigation">
     <nav class="">
   <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-    <div class="relative flex items-center justify-between h-20 min-h-full">
-      <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+    <div class="relative flex items-center justify-center sm:justify-between h-20 min-h-full">
+      <div class="inset-y-0 left-0 flex items-center sm:hidden">
         <!-- Mobile menu button-->
-        <button  @click.prevent="toggleBurger" type="button" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+        <button  @click.prevent="toggleBurger" type="button" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-[#2D635E] focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
           <span class="sr-only">Open main menu</span>
           <!--
             Icon when menu is closed.
@@ -21,7 +21,7 @@
             Icon when menu is open.
 
             Heroicon name: outline/x
-
+        
             Menu open: "block", Menu closed: "hidden"
           -->
           <svg class="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
@@ -29,36 +29,38 @@
           </svg>
         </button>
       </div>
-      <div class="flex-1 flex items-center justify-between ">
-        <div class="flex-shrink-0 flex items-center ">
-          <img class="block lg:hidden  w-auto" src="@/assets/images/logos/logo.svg" alt="logo">
+
+      <div class="sm:flex-1 flex items-center justify-between">
+        <div class="flex-shrink-0 flex items-center">
+          <img class="block lg:hidden w-11 h-8  mx-2 sm:w-auto sm:h-auto  sm:px-2" src="@/assets/images/logos/logo.svg" alt="logo">
 
           <RouterLink to="/"
-              class="text-black hover:bg-[#2D635E] hover:text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page"
-            >Home</RouterLink>
+              class="text-black font-cedarville  h-8   text-[28px] sm:text-[38px] p-1 rounded-md text-sm font-medium" aria-current="page"
+            >Samwayle</RouterLink>
         </div>
+        
         <div class="hidden sm:block sm:ml-6">
           <div class="flex space-x-4">
             <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
 
             <RouterLink to="build"
-              class="text-black hover:bg-[#2D635E] hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+              class="text-black font-josenfin hover:bg-[#2D635E] hover:text-white px-3 py-2 rounded-md text-sm font-medium"
             >About Us</RouterLink>
 
             <RouterLink to="build"
-              class="text-black hover:bg-[#2D635E] hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+              class="text-black font-josenfin  hover:bg-[#2D635E] hover:text-white px-3 py-2 rounded-md text-sm font-medium"
             >Promotion</RouterLink>
 
             <RouterLink to="build"
-              class="text-black hover:bg-[#2D635E] hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+              class="text-black font-josenfin hover:bg-[#2D635E] hover:text-white px-3 py-2 rounded-md text-sm font-medium"
             >Shop</RouterLink>
 
             <RouterLink to="build"
-              class="text-black hover:bg-[#2D635E] hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+              class="text-black font-josenfin hover:bg-[#2D635E] hover:text-white px-3 py-2 rounded-md text-sm font-medium"
             >Contacts</RouterLink>
             
             <RouterLink to="build"
-              class="bg-[#2D635E] hover:text-white text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page"
+              class="bg-[#2D635E] font-josenfin hover:text-white text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page"
             >Contact Us</RouterLink>
 
 
@@ -78,23 +80,23 @@
     class="px-2 pt-2 pb-3 space-y-1 flex-col flex">
       <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
             <RouterLink to="build"
-              class="text-black hover:bg-[#2D635E] hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+              class="text-black font-josenfin text-center hover:bg-[#2D635E] hover:text-white px-3 py-2 rounded-md text-sm font-medium"
             >About Us</RouterLink>
 
             <RouterLink to="build"
-              class="text-black hover:bg-[#2D635E] hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+              class="text-black font-josenfin text-center hover:bg-[#2D635E] hover:text-white px-3 py-2 rounded-md text-sm font-medium"
             >Promotion</RouterLink>
 
             <RouterLink to="build"
-              class="text-black hover:bg-[#2D635E] hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+              class="text-black font-josenfin text-center hover:bg-[#2D635E] hover:text-white px-3 py-2 rounded-md text-sm font-medium"
             >Shop</RouterLink>
 
             <RouterLink to="build"
-              class="text-black hover:bg-[#2D635E] hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+              class="text-black font-josenfin text-center hover:bg-[#2D635E] hover:text-white px-3 py-2 rounded-md text-sm font-medium"
             >Contacts</RouterLink>
             
             <RouterLink to="build"
-              class="bg-[#2D635E] hover:text-white text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page"
+              class="bg-[#2D635E] font-josenfin text-center hover:text-white text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page"
             >Contact Us</RouterLink>
     </div>
   </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,6 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"], 
   theme: {
-    fontFamily: {
-      Cedarville: ["Cedarville-Cursive, sans-serif"],
-    },
     container: {
       padding: "2rem",
       center: true,
@@ -12,6 +9,10 @@ module.exports = {
     screens: {
       sm: "640px",
       md: "768px",
+    },
+    fontFamily: {
+      'cedarville': ['Cedarville Cursive', 'cursive','sans-serif'],
+      'josenfin': ['Josefin Sans', 'sans-serif'],
     },
   },
   plugins: [],
