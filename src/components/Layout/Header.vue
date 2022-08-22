350dfe25ac6757588c8271d6a05a336c129a8e8e
Herman <smurovnemez@mail.ru>
2022-08-22 22:01:20 +0300

    feat: add early header component UI

---
+++ src/components/Layout/Header.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="max-w-7xl md:mx-auto h-[514px]  mx-auto px-2 sm:px-6 lg:px-8">
+        <div class="relative flex items-center  h-20 min-h-full">
+            <div class="flex w-[45%] h-full ">
+            
+            </div>
+
+            <div class=" flex w-[45%] h-full ">
+                
+                    <div class="hidden md:flex items-end  w-full">
+                            
+                        <div class="bg-green-500/[.04] relative flex  items-center justify-center w-[159px] h-[157px] ">
+                            <RouterLink to="/build"
+
+                                class="
+                                p-4 " aria-current="page"
+                                >
+                                <img class="hover:ml-2  hover:w-[35px] hover:h-[8px] duration-200  md:block hidden min-w-[25px] w-[25px] h-[6px]" src="@/assets/images/icons/arrow.png" alt="arrow">
+                            </RouterLink>
+                        </div>   
+                    </div>
+
+                    <div class="flex items-end">
+                        <img class="md:block hidden  max-w-[344px] max-h-[373px]" src="@/assets/images/coffeeGrinder.svg" alt="coffeeGrinder">
+                    </div>
+                
+            </div>
+
+            <div class="flex w-[10%] h-full ">
+            
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,12 @@
 
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <Header />
   </div>
 </template>
 
 <script setup>
+import Header from '../components/Layout/Header.vue';
   
 
 
